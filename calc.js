fc8d7c50b22d93a8c43c040cf9a3e14687f20010
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-27 15:35:27 -0300

    Worked on functions.

--- calc.js
+++ calc.js
@@ -2,55 +2,54 @@
 
 // You will want some kind of html element to display the calculations (maybe a span) and a few buttons to press to input numbers and operators
 
-let input = document.getElementById("Input")
-const divide = document.getElementById("Divide")
-const multiply = document.getElementById("Multiply")
-const subtract = document.getElementById("Subtract")
-const addition = document.getElementById("Addition")
-const seven = document.getElementById("Seven")
-const eight = document.getElementById("Eight")
-const clear = document.getElementById("Clear")
-const four = document.getElementById("Four")
-const five = document.getElementById("Five")
-const six = document.getElementById("Six")
-const allClear = document.getElementById("allClear")
-const one = document.getElementById("One")
-const two = document.getElementById("Two")
-const three = document.getElementById("Three")
-const equal = document.getElementById("Equal")
+let input = document.getElementById("Input");
+const divide = document.getElementById("Divide");
+const multiply = document.getElementById("Multiply");
+const subtract = document.getElementById("Subtract");
+const addition = document.getElementById("Addition");
+const seven = document.getElementById("Seven");
+const eight = document.getElementById("Eight");
+const nine = document.getElementById("Nine");
+const clear = document.getElementById("Clear");
+const four = document.getElementById("Four");
+const five = document.getElementById("Five");
+const six = document.getElementById("Six");
+const allClear = document.getElementById("allClear");
+const one = document.getElementById("One");
+const two = document.getElementById("Two");
+const three = document.getElementById("Three");
+const equal = document.getElementById("Equal");
 
+input.addEventListener("click", useValue);
+divide.addEventListener("click", useValue);
+multiply.addEventListener("click", useValue);
+subtract.addEventListener("click", useValue);
+addition.addEventListener("click", useValue);
+seven.addEventListener("click", useValue);
+eight.addEventListener("click", useValue);
+nine.addEventListener("click", useValue);
+clear.addEventListener("click", clearInput);
+four.addEventListener("click", useValue);
+five.addEventListener("click", useValue);
+six.addEventListener("click", useValue);
+allClear.addEventListener("click", clearAll);
+one.addEventListener("click", useValue);
+two.addEventListener("click", useValue);
+three.addEventListener("click", useValue);
+equal.addEventListener("click", doEquation);
 
-input.addEventListener("Click", useValue)
-divide.addEventListener("Click", useValue)
-multiply.addEventListener("Click", useValue)
-subtract.addEventListener("Click", useValue)
-addition.addEventListener("Click", useValue)
-seven.addEventListener("Click", useValue)
-eight.addEventListener("Click", useValue)
-clear.addEventListener("Click", clearInput)
-four.addEventListener("Click", useValue)
-five.addEventListener("Click", useValue)
-six.addEventListener("Click", useValue)
-allClear.addEventListener("Click", clearAll)
-one.addEventListener("Click", useValue)
-two.addEventListener("Click", useValue)
-three.addEventListener("Click", useValue)
-equal.addEventListener("Click", doEquation)
-
-
-
-function useValue() {
-    console.log("Dog")
+function useValue(event) {
+  input.value += event.target.innerHTML;
 }
 
 function clearInput() {
-    console.log("Clear")
+  input.value = "";
 }
 
 function clearAll() {
-    console.log("All Clear")
+  console.log("All Clear");
 }
 
 function doEquation() {
-    console.log("Equals")
+  console.log("Equals");
 }
