334f9e242b6fcfa08b5d1ec2e52d8cb5448938d9
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-27 14:54:18 -0300

    Created constants for each operator.

--- calc.js
+++ calc.js
@@ -1,3 +1,20 @@
 // Build a simple webpage that has an interactive calculator using JavaScript
 
 // You will want some kind of html element to display the calculations (maybe a span) and a few buttons to press to input numbers and operators
+
+let input = document.getElementById("input")
+const divide = document.getElementById("Divide")
+const multiply = document.getElementById("Multiply")
+const subtract = document.getElementById("Subtract")
+const Addition = document.getElementById("Addition")
+const seven = document.getElementById("Seven")
+const eight = document.getElementById("Eight")
+const clear = document.getElementById("Clear")
+const Four = document.getElementById("Four")
+const five = document.getElementById("Five")
+const six = document.getElementById("Six")
+const allClear = document.getElementById("allClear")
+const one = document.getElementById("One")
+const two = document.getElementById("Two")
+const three = document.getElementById("Three")
+const equal = document.getElementById("Equal")
