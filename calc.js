6555cf7e46036822f71e362a3f6701f64b55c22b
Matt Oake <matt@Matts-MacBook-Air.local>
2022-10-27 13:58:00 -0300

    First commit.

---
+++ calc.js
@@ -0,0 +1,3 @@
+// Build a simple webpage that has an interactive calculator using JavaScript
+
+// You will want some kind of html element to display the calculations (maybe a span) and a few buttons to press to input numbers and operators
