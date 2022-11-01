a6194796f9c91d68cb8d66b444550bd28531819a
Matt Oake <matt@Matts-MacBook-Air.local>
2022-11-01 14:46:41 -0300

    Made a class for digits and operators, removed each individual const for these buttons so that they could live inside an array using document.querySelectorAll for a new const calcButtons, Made a function that loops through this array and does .addEventListener for when this class of buttons is clicked on to use its value.

--- calc.js
+++ calc.js
@@ -4,42 +4,19 @@
 
 let input = document.getElementById("Input");
 let history = "";
-const divide = document.getElementById("Divide");
-const multiply = document.getElementById("Multiply");
-const subtract = document.getElementById("Subtract");
-const addition = document.getElementById("Addition");
-const seven = document.getElementById("Seven");
-const eight = document.getElementById("Eight");
-const nine = document.getElementById("Nine");
 const clear = document.getElementById("Clear");
-const four = document.getElementById("Four");
-const five = document.getElementById("Five");
-const six = document.getElementById("Six");
 const allClear = document.getElementById("allClear");
-const one = document.getElementById("One");
-const two = document.getElementById("Two");
-const three = document.getElementById("Three");
 const equal = document.getElementById("Equal");
-const zero = document.getElementById("Zero");
+const calcButtons = document.querySelectorAll(".calcButton");
 
-input.addEventListener("click", useValue);
-divide.addEventListener("click", useValue);
-multiply.addEventListener("click", useValue);
-subtract.addEventListener("click", useValue);
-addition.addEventListener("click", useValue);
-seven.addEventListener("click", useValue);
-eight.addEventListener("click", useValue);
-nine.addEventListener("click", useValue);
 clear.addEventListener("click", clearInput);
-four.addEventListener("click", useValue);
-five.addEventListener("click", useValue);
-six.addEventListener("click", useValue);
 allClear.addEventListener("click", clearAll);
-one.addEventListener("click", useValue);
-two.addEventListener("click", useValue);
-three.addEventListener("click", useValue);
 equal.addEventListener("click", doEquation);
-zero.addEventListener("click", useValue);
+calcButtons.forEach(addEventListenerToCalcButtons);
+
+function addEventListenerToCalcButtons(calcButton) {
+  calcButton.addEventListener("click", useValue);
+}
 
 function addToHistory(value) {
   history += value;
